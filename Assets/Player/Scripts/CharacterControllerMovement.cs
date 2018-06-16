0ac1ffbf102aadf67df5aaf7a00e58b411b77877
Yamaraj <aleksander.gorka@gmail.com>
2018-06-16 21:00:08 +0200

    Player Controller

---
+++ Assets/Player/Scripts/CharacterControllerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterControllerMovement : MonoBehaviour
+{
+	//Components.
+	private Rigidbody rb;
+	private CapsuleCollider collider;
+	private Animator animator;
+	private CharacterControllerRB characterController;
+
+	//Moving 
+	public float idleStartTime = 0.2f;
+	private float stopTime = 0;
+	public float runSpeed = 200f;
+
+	//Animator const.
+	public float damping = 0.15f;
+
+	//Input variables.
+	float inputVertical = 0f;
+	private Vector3 inputVec;
+
+	// Use this for initialization
+	void Start ()
+	{
+		characterController = GetComponentInChildren<CharacterControllerRB>();
+		rb = GetComponentInChildren<Rigidbody>();
+		collider = GetComponent<CapsuleCollider>();
+		animator = GetComponentInChildren<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!characterController.isBusy)
+        {
+            Inputs();
+            Move();
+        }
+        else
+        {
+            MovePhysic(new Vector3(0,0,0),0);
+        }
+    }
+
+	void Inputs()
+	{
+	//	inputHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		inputVertical = CrossPlatformInputManager.GetAxis("Vertical");
+		inputVec = new Vector3(0, 0, inputVertical).normalized;
+	}
+
+	private void Move()
+	{		
+		if (characterController.isGrounded && inputVertical != 0 && inputVec.z>0)
+		{
+			//Move Animation
+			animator.SetBool("Moving", true);
+			animator.SetFloat("MoveSpeed", inputVec.z, damping, Time.deltaTime);
+			stopTime = 0;
+
+			MovePhysic(inputVec, runSpeed);
+		}
+		else
+		{
+			if (stopTime > idleStartTime)
+			{
+				animator.SetBool("Moving", false);
+			}
+			stopTime += Time.deltaTime;
+            MovePhysic(new Vector3(0,0,0),0);
+		}
+	}
+
+	private void MovePhysic(Vector3 inputVec, float speed)
+	{
+		Vector3 motion = Vector3.zero;
+		motion = new Vector3(inputVec.x, 0, inputVec.z);
+		motion = transform.TransformDirection(motion) * speed;
+		rb.velocity = new Vector3(motion.x * Time.deltaTime,rb.velocity.y, motion.z * Time.deltaTime);
+	}
+}
