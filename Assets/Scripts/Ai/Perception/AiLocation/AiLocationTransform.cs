e76b4d53dbd4cff7c758a5397945951a3deeb3ad
Risist <dmnn@op.pl>
2018-06-16 20:48:50 +0200

    Ai Base commit

---
+++ Assets/Scripts/Ai/Perception/AiLocation/AiLocationTransform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiLocationTransform : AiLocationBase{
+
+	public Transform aim;
+
+	new public void Start()
+	{
+		base.Start();
+		if (!aim)
+			aim = transform;
+	}
+
+	public override Vector2 GetLocation()
+	{
+		return base.GetLocation() + new Vector2(aim.position.x, aim.position.z);
+	}
+	public override bool IsValid()
+	{
+		return aim && base.IsValid();
+	}
+}
---
+++ Assets/Scripts/Audio/AwakeAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAudio : MonoBehaviour {
+
+	public AudioClip audioClip;
+	public float pitchBase = 1.0f;
+	public float pitchRandom = 0.0f;
+	public float volumeBase = 1.0f;
+	public float volumeRandom = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		AudioManager.inst.CreateInstance(audioClip, transform.position,
+			volumeBase + (Random.value - 0.5f) * 2.0f * volumeRandom,
+			pitchBase + (Random.value - 0.5f) * 2.0f * pitchRandom
+		);
+	}
+}
