90241944288a617fa270a0583506856f45a95f6b
[Maciej Kamieniak] <[strosx@gmail.com]>
2018-06-16 22:05:43 +0200

    throw seed

--- Assets/Scripts/Seeds/SeedController.cs
+++ Assets/Scripts/Seeds/SeedController.cs
@@ -22,6 +22,7 @@ namespace SkilPoint_Game_Jam.Assets.Scripts
         {
             if (other.gameObject.tag == "ground")
             {
+          //      yield return new WaitForSeconds(2);
                 CreateTree ();
                 yield return StartCoroutine (ScaleOverTime (_growingSpeed));
             }
@@ -50,7 +51,6 @@ namespace SkilPoint_Game_Jam.Assets.Scripts
                 yield return null;
             } while (currentTime <= time);
 
-            gameObject.SetActive(false);
             DestroyImmediate (gameObject);
         }
 
