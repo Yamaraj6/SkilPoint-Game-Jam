15c2e812615c8eeb31e2c97502fc27b0e70946fb
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-06-17 11:14:47 +0200

    new Skill

---
+++ Assets/BodyPartsController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(HealthController))]
+public class BodyPartsController : MonoBehaviour {
+    public GameObject[] bodyParts;
+    public GameObject bodyCutEffect;
+    private List<GameObject> listBodyParts;
+    HealthController healthController;
+    int legsPartFlag = 2;
+
+    // Use this for initialization
+    void Start () {
+        healthController = GetComponent<HealthController>();
+        listBodyParts = bodyParts.OfType<GameObject>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckHealth();
+    }
+
+    private void CheckHealth()
+    {
+        if (healthController.actual < healthController.max * 0.8f && legsPartFlag == 2)
+        {
+            CutBodyPart();
+        }
+        else if (healthController.actual < healthController.max * 0.4f && legsPartFlag == 1)
+        {
+            CutBodyPart();
+        }
+    }
+
+    private void CutBodyPart()
+    {
+        if (listBodyParts[0] != null)
+        {
+            listBodyParts[0].transform.localScale = new Vector3(0, 0, 0);
+            RespEffect(listBodyParts[0].transform);
+            listBodyParts.RemoveAt(0);
+            legsPartFlag--;
+        }
+    }
+
+    private void RespEffect(Transform transform)
+    {
+    var effect = GameObject.Instantiate(bodyCutEffect, 
+        transform.position, transform.rotation);
+        Destroy(effect,2);
+    }
+}
